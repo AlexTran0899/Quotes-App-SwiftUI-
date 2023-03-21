5eef9e7d78ceb4c85c07fd2febcba92bfad62577
Alextran0899 <Alextran0899@gmail.com>
2023-03-21 18:22:36 -0500

    added a fun file

---
+++ QuotesApp/Views/FunView.swift
@@ -0,0 +1,20 @@
+//
+//  FunView.swift
+//  QuotesApp
+//
+//  Created by Alex Tran on 3/21/23.
+//
+
+import SwiftUI
+
+struct FunView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FunView_Previews: PreviewProvider {
+    static var previews: some View {
+        FunView()
+    }
+}
